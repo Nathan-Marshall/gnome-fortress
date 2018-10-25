cd60db224047bf9f277dba912e9e520e0ce218ad
Mitchell Blanchard <mitchellblanchard3@gmail.com>
2018-10-25 11:35:50 -0400

    Added the skeletons for the enemies class, and the basic enemy class which will be a be a subclass of a basicMeshNode

---
+++ include/gnome_fortress/game/Enemies.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+
+namespace gnome_fortress {
+namespace game {
+
+
+class Enemies {
+
+public:
+	Enemies();
+
+protected:
+
+private:
+
+};
+
+
+}
+}
