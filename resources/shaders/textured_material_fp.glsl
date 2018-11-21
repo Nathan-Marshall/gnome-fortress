3aa0af379bb93212548bc7a79b74fee0d08c4942
Nathan Marshall <NathanMarshall@cmail.carleton.ca>
2018-11-20 20:43:17 -0500

    BasicMeshNodeTechnique and the textured_material shader now uses many of the features from the MTL file
    The values currently used are:
    
    Ns: specular_exponent (phong shading)
    
    Kd: diffuse_color (If there is a map_Kd, each color component will be multiplied by the component from this color, so it should pretty much always be white. We currently ignore Ka and map_Ka, so ambient light uses this too.)
    
    Ks: specular_color (Multiplies with map_Ks just like Kd does with map_Kd.)
    
    map_Kd: diffuse_map (If missing texture, diffuse_map_on will be 0, using Kd directly.)
    
    map_Ks: specular_map (If missing texture, specular_map_on will be 0, using Ks directly.)
    
    This is pretty much all I want to implement for materials, except for normal maps, and possibly reflectivity at some point. I will do normal maps next, and get us a nice wall model.

--- resources/shaders/textured_material_fp.glsl
+++ resources/shaders/textured_material_fp.glsl
@@ -16,12 +16,20 @@ in vec4 color_interp;
 in vec2 uv_interp;
 
 // global uniforms
-uniform vec3 sun_dir = vec3(0, 1f, 1f);
+uniform vec3 sun_dir = vec3(0, 1, 1);
+uniform vec3 sun_color = vec3(1, 0.98, 0.95);
+
+uniform vec3 diffuse_color = vec3(1, 1, 1);
+uniform vec3 specular_color = vec3(0, 0, 0);
+uniform float specular_exponent = 100.0;
+
 uniform sampler2D diffuse_map;
+uniform int diffuse_map_on = 0;
+uniform sampler2D gloss_map;
+uniform int gloss_map_on = 0;
 
 // Material attributes (constants)
-float ambient_intensity = 0.1f;
-float phong_exponent = 100.0;
+float ambient_intensity = 0.1;
 
 struct BlinnPhongResult {
     float diffuse_intensity;
@@ -45,21 +53,25 @@ BlinnPhongResult calculateBlinnPhong(vec3 light_dir) {
     vec3 H = normalize(V + L); // halfway vector (between view and light)
 
     float spec_angle_cos = max(dot(N, H), 0.0);
-    float Is = pow(spec_angle_cos, phong_exponent);
+    float Is = pow(spec_angle_cos, specular_exponent);
 
     return BlinnPhongResult(Id, Is);
 }
 
+vec3 multiplyColors(vec3 c1, vec3 c2) {
+    return vec3(c1[0] * c2[0], c1[1] * c2[1], c1[2] * c2[2]);
+}
+
 void main() 
 {
-    vec3 ambient_color = vec3(texture(diffuse_map, uv_interp)) * ambient_intensity;
-    vec3 diffuse_color = vec3(texture(diffuse_map, uv_interp));
-    vec3 specular_color = vec3(1, 1, 1);
+    vec3 diffuse_color_final = multiplyColors( vec3(texture(diffuse_map, uv_interp)) * diffuse_map_on + vec3(1, 1, 1) * (1 - diffuse_map_on), diffuse_color );
+    vec3 ambient_color_final = diffuse_color_final * ambient_intensity;
+    vec3 specular_color_final = multiplyColors( vec3(texture(gloss_map, uv_interp)) * gloss_map_on + vec3(1, 1, 1) * (1 - gloss_map_on), specular_color );
 
-    //BlinnPhongResult blinn_phong = calculateBlinnPhong(light_pos - position_interp);
-    BlinnPhongResult blinn_phong = calculateBlinnPhong(sun_dir);
-	vec4 final_color = vec4(ambient_color + blinn_phong.diffuse_intensity*diffuse_color + blinn_phong.specular_intensity*specular_color, 1);
+    //BlinnPhongResult blinn_phong_result = calculateBlinnPhong(light_pos - position_interp);
+    BlinnPhongResult blinn_phong_result = calculateBlinnPhong(sun_dir);
+	vec4 blinn_phong_color = vec4(multiplyColors( ambient_color_final + blinn_phong_result.diffuse_intensity*diffuse_color_final + blinn_phong_result.specular_intensity*specular_color_final, sun_color ), 1);
     
     // Assign light to the fragment
-    gl_FragColor = final_color;
+    gl_FragColor = blinn_phong_color;
 }
--- src/gnome_fortress/game/main.cpp
+++ src/gnome_fortress/game/main.cpp
@@ -251,7 +251,7 @@ int MainFunction(void){
 
         // Set up shaders
         GLuint program = resource_manager_g.getOrLoadShaderProgram(resources::shaders::textured_material);
-        auto technique = new renderer::BasicMeshNodeTechnique(program, "projection_mat", "view_mat", "world_mat", "normal_mat", "eye_pos", "diffuse_map");
+        auto technique = new renderer::BasicMeshNodeTechnique(program, "projection_mat", "view_mat", "world_mat", "normal_mat", "eye_pos", "diffuse_color", "specular_color", "specular_exponent", "diffuse_map", "diffuse_map_on", "gloss_map", "gloss_map_on");
         technique->addVertexAttribute(renderer::VertexAttribute(program, "vertex", 3, GL_FLOAT, GL_FALSE));
         technique->addVertexAttribute(renderer::VertexAttribute(program, "normal", 3, GL_FLOAT, GL_FALSE));
         technique->addVertexAttribute(renderer::VertexAttribute(program, "color", 3, GL_FLOAT, GL_FALSE));
